880e1a702206daaeb8d093ecc3f5a5b611aeb9e7
Brandon Lau <brandon.lau86@gmail.com>
2020-09-05 20:06:41 -0400

    add interfaces to calculate

--- src/stores/DataStore.ts
+++ src/stores/DataStore.ts
@@ -1,13 +1,62 @@
-import { observable, computed } from "mobx";
+import { observable } from "mobx";
 import { DSVRowArray } from "d3";
+
+interface DTURetrace {
+  downTwentyFive: number;
+  downFifty: number;
+}
+
+interface UTDRetrace {
+  upTwentyFive: number;
+  upFifty: number;
+}
+
+interface AllCloseProbabilities {
+  upOnePlus: number;
+  upSeventyFiveToOne: number;
+  upFiftyToSeventyFive: number;
+  upTwentyFiveToFifty: number;
+  upZeroToTwentyFive: number;
+  downZeroToTwentyFive: number;
+  downTwentyFiveToFifty: number;
+  downFiftyToSeventyFive: number;
+  downSeventyFiveToOne: number;
+  downOnePlus: number;
+}
 
 export class DataStore {
   @observable name: string = "";
 
+  /*
+  Change %
+  Date
+  High
+  Low 
+  Open 
+  Price
+  */
   @observable rawData: DSVRowArray<string> | null[] = [];
 
   @observable totalUpDays: number = 0;
   @observable totalDownDays: number = 0;
+
+  @observable downToUpRetrace: DTURetrace = {
+    downTwentyFive: 0,
+    downFifty: 0,
+  };
+
+  @observable allCloseProbabilities: AllCloseProbabilities = {
+    upOnePlus: 0,
+    upSeventyFiveToOne: 0,
+    upFiftyToSeventyFive: 0,
+    upTwentyFiveToFifty: 0,
+    upZeroToTwentyFive: 0,
+    downZeroToTwentyFive: 0,
+    downTwentyFiveToFifty: 0,
+    downFiftyToSeventyFive: 0,
+    downSeventyFiveToOne: 0,
+    downOnePlus: 0,
+  };
 
   parseData = () => {
     this.rawData.forEach((data: any) => {

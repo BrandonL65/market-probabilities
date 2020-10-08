c435f82ac6c4caa54f12f46e60705343d272f8a7
Brandon Lau <brandonlau@Brandons-MBP.fios-router.home>
2020-10-08 05:04:46 -0400

    added interface for candle proportions, and will parse green candles next

--- src/stores/DataStore.ts
+++ src/stores/DataStore.ts
@@ -50,6 +50,106 @@ export interface CandleRangeStatistics {
   u250plus: number;
 }
 
+//Interface for the candle proportions, like open to low, open to high, open to close ranges
+interface CandleProportions {
+  OH5: number;
+  OH10: number;
+  OH15: number;
+  OH20: number;
+  OH25: number;
+  OH30: number;
+  OH35: number;
+  OH40: number;
+  OH45: number;
+  OH50: number;
+  OH55: number;
+  OH60: number;
+  OH70: number;
+  OH80: number;
+  OH90: number;
+  OH100: number;
+  OH110: number;
+  OH120: number;
+  OH130: number;
+  OH140: number;
+  OH150: number;
+  OH160: number;
+  OH170: number;
+  OH180: number;
+  OH190: number;
+  OH200: number;
+  OH210: number;
+  OH220: number;
+  OH230: number;
+  OH240: number;
+  OH250: number;
+  OH250plus: number;
+  OC5: number;
+  OC10: number;
+  OC15: number;
+  OC20: number;
+  OC25: number;
+  OC30: number;
+  OC35: number;
+  OC40: number;
+  OC45: number;
+  OC50: number;
+  OC55: number;
+  OC60: number;
+  OC70: number;
+  OC80: number;
+  OC90: number;
+  OC100: number;
+  OC110: number;
+  OC120: number;
+  OC130: number;
+  OC140: number;
+  OC150: number;
+  OC160: number;
+  OC170: number;
+  OC180: number;
+  OC190: number;
+  OC200: number;
+  OC210: number;
+  OC220: number;
+  OC230: number;
+  OC240: number;
+  OC250: number;
+  OC25OPlus: number;
+  OL5: number;
+  OL10: number;
+  OL15: number;
+  OL20: number;
+  OL25: number;
+  OL30: number;
+  OL35: number;
+  OL40: number;
+  OL45: number;
+  OL50: number;
+  OL55: number;
+  OL60: number;
+  OL70: number;
+  OL80: number;
+  OL90: number;
+  OL100: number;
+  OL110: number;
+  OL120: number;
+  OL130: number;
+  OL140: number;
+  OL150: number;
+  OL160: number;
+  OL170: number;
+  OL180: number;
+  OL190: number;
+  OL200: number;
+  OL210: number;
+  OL220: number;
+  OL230: number;
+  OL240: number;
+  OL250: number;
+  OL25OPlus: number;
+}
+
 export class DataStore {
   @observable name: string = "";
 
@@ -116,6 +216,106 @@ export class DataStore {
     u250plus: 0,
   };
 
+  @observable CandleProportionsUpCandles: CandleProportions = {
+    OH5: 0,
+    OH10: 0,
+    OH15: 0,
+    OH20: 0,
+    OH25: 0,
+    OH30: 0,
+    OH35: 0,
+    OH40: 0,
+    OH45: 0,
+    OH50: 0,
+    OH55: 0,
+    OH60: 0,
+    OH70: 0,
+    OH80: 0,
+    OH90: 0,
+    OH100: 0,
+    OH110: 0,
+    OH120: 0,
+    OH130: 0,
+    OH140: 0,
+    OH150: 0,
+    OH160: 0,
+    OH170: 0,
+    OH180: 0,
+    OH190: 0,
+    OH200: 0,
+    OH210: 0,
+    OH220: 0,
+    OH230: 0,
+    OH240: 0,
+    OH250: 0,
+    OH250plus: 0,
+    OC5: 0,
+    OC10: 0,
+    OC15: 0,
+    OC20: 0,
+    OC25: 0,
+    OC30: 0,
+    OC35: 0,
+    OC40: 0,
+    OC45: 0,
+    OC50: 0,
+    OC55: 0,
+    OC60: 0,
+    OC70: 0,
+    OC80: 0,
+    OC90: 0,
+    OC100: 0,
+    OC110: 0,
+    OC120: 0,
+    OC130: 0,
+    OC140: 0,
+    OC150: 0,
+    OC160: 0,
+    OC170: 0,
+    OC180: 0,
+    OC190: 0,
+    OC200: 0,
+    OC210: 0,
+    OC220: 0,
+    OC230: 0,
+    OC240: 0,
+    OC250: 0,
+    OC25OPlus: 0,
+    OL5: 0,
+    OL10: 0,
+    OL15: 0,
+    OL20: 0,
+    OL25: 0,
+    OL30: 0,
+    OL35: 0,
+    OL40: 0,
+    OL45: 0,
+    OL50: 0,
+    OL55: 0,
+    OL60: 0,
+    OL70: 0,
+    OL80: 0,
+    OL90: 0,
+    OL100: 0,
+    OL110: 0,
+    OL120: 0,
+    OL130: 0,
+    OL140: 0,
+    OL150: 0,
+    OL160: 0,
+    OL170: 0,
+    OL180: 0,
+    OL190: 0,
+    OL200: 0,
+    OL210: 0,
+    OL220: 0,
+    OL230: 0,
+    OL240: 0,
+    OL250: 0,
+    OL25OPlus: 0,
+  };
+
+  //parses ALL data by looping through the raw data
   parseData = () => {
     console.log("Parsing data from dataStore");
     this.rawData.forEach((data: any) => {
@@ -129,16 +329,35 @@ export class DataStore {
       this.testClosingZone(open, close, change);
       this.testCandleRange(high, low);
       this.averageRange = this.averageRange + (high - low) * 10000;
+
+      //parsed O-C, O-H, O-L percentages for all green candles
+      if (open < close) {
+        this.testCandleProportionPercentagesUpCandles(open, high, low, close);
+      }
     });
 
     //calculates average high-low range, assigns it
     this.averageRange = parseFloat(
       (this.averageRange / this.rawData.length).toFixed(2)
     );
-    console.log(this.allRanges);
   };
 
-  //test the % of time price retraces X%, then closes opposite direction
+  //does calculations for OC OH OL of green candles
+  testCandleProportionPercentagesUpCandles = (
+    open: number,
+    high: number,
+    low: number,
+    close: number
+  ) => {
+    let highToOpenDifference = Math.floor((high - open) * 10000);
+    let closeToOpenDifference = Math.floor((close - open) * 10000);
+    let openToLowDifference = Math.floor((open - low) * 10000);
+    console.log(
+      highToOpenDifference,
+      closeToOpenDifference,
+      openToLowDifference
+    );
+  };
 
   //test probabilities to close within 0.25% "zones" using close price relative to open
   testClosingZone = (open: number, close: number, change: number) => {

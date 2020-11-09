d26de563d2d985f0cad78c01a2ed0cb8056cb9d6
Brandon Lau <brandonlau@Brandons-MBP.fios-router.home>
2020-11-09 03:21:44 -0500

    add 1h parsing for percentcloseredgreenaftermoveofcertainamount

--- src/stores/DataStore.ts
+++ src/stores/DataStore.ts
@@ -512,39 +512,6 @@ export class DataStore {
     this.numTimesPriceWentBackToOpenAfterXPipsOneSide();
   };
 
-  @observable total1HCandles = 0;
-
-  parse1HData = (csvFile: DSVRowArray<string>) => {
-    let all1HDays = new Map<string, CandleStick[]>();
-    let candlesFromSameDay: CandleStick[] = [];
-    let currentDay = csvFile[0]["Local time"]!;
-
-    for (let candle of csvFile) {
-      this.total1HCandles++;
-      let candleTime = candle["Local time"]!;
-      let hour = candleTime.split(" ")[1].split(":")[0];
-      let minute = candleTime.split(" ")[1].split(":")[1];
-
-      if (hour === "16" && minute === "00") {
-        currentDay = candleTime;
-        all1HDays.set(currentDay, candlesFromSameDay);
-        candlesFromSameDay = [];
-        this.total5mDays++;
-        continue;
-      }
-      let candleObject: CandleStick = {
-        Open: parseFloat(candle["Open"]!),
-        High: parseFloat(candle["High"]!),
-        Low: parseFloat(candle["Low"]!),
-        Close: parseFloat(candle["Close"]!),
-        Time: candle["Local time"]!,
-      };
-      candlesFromSameDay.push(candleObject);
-    }
-    this.sorted1HData = all1HDays;
-    console.log(all1HDays);
-    // this.enterAfterPriceFallsXPips();
-  };
   numTimesPriceWentBackToOpenAfterXPipsOneSide = () => {
     let results = {
       total: 0,
@@ -581,6 +548,182 @@ export class DataStore {
       }
     }
     console.log(results);
+  };
+
+  @observable total1HCandles = 0;
+  @observable total1HDays = 0;
+  parse1HData = (csvFile: DSVRowArray<string>) => {
+    let all1HDays = new Map<string, CandleStick[]>();
+    let candlesFromSameDay: CandleStick[] = [];
+    let currentDay = csvFile[0]["Local time"]!;
+
+    for (let candle of csvFile) {
+      this.total1HCandles++;
+      let candleTime = candle["Local time"]!;
+      let hour = candleTime.split(" ")[1].split(":")[0];
+      let minute = candleTime.split(" ")[1].split(":")[1];
+      let candleObject: CandleStick = {
+        Open: parseFloat(candle["Open"]!),
+        High: parseFloat(candle["High"]!),
+        Low: parseFloat(candle["Low"]!),
+        Close: parseFloat(candle["Close"]!),
+        Time: candle["Local time"]!,
+      };
+      candlesFromSameDay.push(candleObject);
+      if (hour === "16" && minute === "00") {
+        currentDay = candleTime;
+        all1HDays.set(currentDay, candlesFromSameDay);
+        candlesFromSameDay = [];
+        this.total1HDays++;
+        continue;
+      }
+    }
+    this.sorted1HData = all1HDays;
+    console.log(all1HDays);
+    this.PercentCloseGreenRedAfterMovementOfCertainAmt();
+  };
+
+  PercentCloseGreenRedAfterMovementOfCertainAmt = () => {
+    let data = new Map([
+      ["totalDays", 0],
+      ["totalGreenDays", 0],
+      ["totalRedDays", 0],
+      ["totalTouchedDev", 0],
+      ["totalClosedGreenAfterDev", 0],
+      ["totalClosedRedAfterDev", 0],
+      ["totalTouchedOpenAgainAfterDev", 0],
+      ["touchedOpenAndClosedGreen", 0],
+      ["touchedOpenAndClosedRed", 0],
+      ["wentBackToDevAgain", 0],
+      ["wentLowerByDeviation", 0],
+    ]);
+    for (let [date, candlesArr] of this.sorted1HData) {
+      let dailyOpen = candlesArr[0].Open;
+      let dailyClose = candlesArr[candlesArr.length - 1].Close;
+      let deviationUp = dailyOpen + 0.004;
+      let deviationDown = dailyOpen - 0.004;
+      data.set("totalDays", data.get("totalDays")! + 1);
+      if (dailyClose > dailyOpen) {
+        data.set("totalGreenDays", data.get("totalGreenDays")! + 1);
+      } else {
+        data.set("totalRedDays", data.get("totalRedDays")! + 1);
+      }
+
+      for (let i = 0; i < candlesArr.length; i++) {
+        if (candlesArr[i].High >= deviationUp) {
+          data.set("totalTouchedDev", data.get("totalTouchedDev")! + 1);
+          if (dailyClose > dailyOpen) {
+            data.set(
+              "totalClosedGreenAfterDev",
+              data.get("totalClosedGreenAfterDev")! + 1
+            );
+          } else {
+            data.set(
+              "totalClosedRedAfterDev",
+              data.get("totalClosedRedAfterDev")! + 1
+            );
+          }
+          for (let j = i + 1; j < candlesArr.length; j++) {
+            if (candlesArr[j].Low <= dailyOpen) {
+              data.set(
+                "totalTouchedOpenAgainAfterDev",
+                data.get("totalTouchedOpenAgainAfterDev")! + 1
+              );
+              if (dailyClose > dailyOpen) {
+                data.set(
+                  "touchedOpenAndClosedGreen",
+                  data.get("touchedOpenAndClosedGreen")! + 1
+                );
+              } else {
+                data.set(
+                  "touchedOpenAndClosedRed",
+                  data.get("touchedOpenAndClosedRed")! + 1
+                );
+              }
+              for (let k = j + 1; k < candlesArr.length; k++) {
+                if (candlesArr[k].High >= deviationUp) {
+                  data.set(
+                    "wentBackToDevAgain",
+                    data.get("wentBackToDevAgain")! + 1
+                  );
+                  break;
+                } else if (candlesArr[k].Low <= deviationDown) {
+                  data.set(
+                    "wentLowerByDeviation",
+                    data.get("wentLowerByDeviation")! + 1
+                  );
+                  break;
+                }
+              }
+              break;
+            }
+          }
+          break;
+        }
+      }
+    }
+    console.log(data);
+  };
+
+  likelihoodOfComingBackToOpen = () => {
+    let trades = {
+      totalDays: 0,
+      totalUpDays: 0,
+      totalDownDays: 0,
+      touchedLowerDeviation: 0,
+      upDaysTouchedLowerDeviation: 0,
+      downDaysTouchedLowerDeviation: 0,
+      totalBounceBackToOpenAfterLowerDevTouch: 0,
+      wentToUDafterOpen: 0,
+      wentToLDafterOpen: 0,
+    };
+    for (let [k, candlesArr] of this.sorted1HData) {
+      let dailyOpen = candlesArr[0].Open;
+      let dailyClose = candlesArr[candlesArr.length - 1].Close;
+      let upDay = dailyClose > dailyOpen ? true : false;
+      let lowerDeviation = dailyOpen - 0.005;
+      let upperDeviation = dailyOpen + 0.005;
+
+      trades.totalDays++;
+      if (upDay) {
+        trades.totalUpDays++;
+      } else {
+        trades.totalDownDays++;
+      }
+
+      for (let i = 0; i < candlesArr.length; i++) {
+        if (candlesArr[i].Low <= lowerDeviation) {
+          trades.touchedLowerDeviation++;
+          if (upDay) {
+            trades.upDaysTouchedLowerDeviation++;
+          } else if (!upDay) {
+            trades.downDaysTouchedLowerDeviation++;
+          }
+          for (let j = i + 1; j < candlesArr.length; j++) {
+            if (candlesArr[j].High >= dailyOpen) {
+              trades.totalBounceBackToOpenAfterLowerDevTouch++;
+              for (let k = j + 1; k < candlesArr.length; k++) {
+                if (
+                  candlesArr[k].Low <= lowerDeviation &&
+                  candlesArr[k].High >= upperDeviation
+                ) {
+                  break;
+                } else if (candlesArr[k].Low <= lowerDeviation) {
+                  trades.wentToLDafterOpen++;
+                  break;
+                } else if (candlesArr[k].High >= upperDeviation) {
+                  trades.wentToUDafterOpen++;
+                  break;
+                }
+              }
+              break;
+            }
+          }
+          break;
+        }
+      }
+    }
+    console.log(trades);
   };
 
   //when price falls 30 pips, enter with SL at X and TP at Y, see % of wins, etc
